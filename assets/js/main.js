2eb4557a8383e9b0b17cee990f98c56423691025
Chandrashekhar Robbi <chandrashekarrobbi789@gmail.com>
2023-02-22 15:11:12 +0530

    changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -158,7 +158,16 @@ function scrollUp(){
 }
 window.addEventListener('scroll', scrollUp)
 
-
+/*====================Contact Me ========================*/
+const scriptURL = "https://script.google.com/macros/s/AKfycbw7LD9IyH8LnLnqOHhKmOvRz9vR4IGs7Q67wvfZW5Ir4unBFdV_DquuBe-FS1t89DN3iw/exec";
+const form = document.forms['submit-to-google-sheet'];
+const msg = document.getElementById('contact-reply');
+form.addEventListener('submit', async e => {
+    e.preventDefault();
+    fetch(scriptURL, { method: 'POST', body: new FormData(form) });
+    msg.innerHTML = "Thank you! I will be in touch with you soon.✌🏻";
+    document.getElementById("myform").reset();
+});
 
 /*==================== DARK LIGHT THEME ====================*/ 
 
