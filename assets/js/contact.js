e2343c5c88e0fc7e09cb0739f3044a9568d57970
Chandrashekhar Robbi <chandrashekarrobbi789@gmail.com>
2023-02-22 15:21:00 +0530

    changes

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -4,7 +4,6 @@ const msg = document.getElementById('contact-reply');
 form.addEventListener('submit', async e => {
     e.preventDefault();
     fetch(scriptURL, { method: 'POST', body: new FormData(form) });
-    //   console.log(data);
     msg.innerHTML = "Thank you! I will be in touch with you soon.✌🏻";
     document.getElementById("myform").reset();
 });
